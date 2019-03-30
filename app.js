9e5b182c1f6bdc5c1e3e1f30f98bf5b29387802a
kitcolmx <kitcolmx@gmail.com>
2019-03-30 21:42:21 +0900

    Building the Real Application

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var fs = require('file-system');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const fs = require('file-system');
 
-var app = express();
+const app = express();
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost:27017/tutorial2', {
+mongoose.connect('mongodb://localhost:17017/tutorial2', {
 });
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
   console.log("Connection Succeeded");
 });
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Include controllers
 fs.readdirSync('controllers').forEach(file => {
   if(file.substr(-3) == '.js') {
     const route = require('./controllers/' + file);
@@ -29,19 +39,11 @@ fs.readdirSync('controllers').forEach(file => {
   }
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,56 @@
+const User = require("../models/User");
+
 module.exports.controller = (app) => {
+  // get all users
   app.get('/users', (req, res) => {
-    res.render('index', {title: 'Users'});
+    User.find({}, 'name email', function (error, users) {
+      if (error) { console.log(error); }
+      res.send(users);
+    })
+  })
+
+  //get a single user details
+  app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id, 'name email', function (error, user) {
+      if (error) { console.log(error); }
+      res.send(user)
+    })
+  })
+
+  // add a new user
+  app.post('/users', (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email
+    })
+
+    user.save(function (error, user) {
+      if (error) { console.log(error); }
+      res.send(user)
+    })
+  })
+
+  // update a user
+  app.put('/users/:id', (req, res) => {
+    User.findById(req.params.id, 'name email', function (error, user) {
+      if (error) { console.error(error); }
+
+      user.name = req.body.name
+      user.email = req.body.email
+      user.save(function (error, user) {
+        if (error) { console.log(error); }
+        res.send(user)
+      })
+    })
+  })
+
+  // delete a user
+  app.delete('/users/:id', (req, res) => {
+    User.remove({
+      _id: req.params.id
+    }, function(error, user){
+      if (error) { console.error(error); }
+      res.send({ success: true })
+    })
   })
 }
